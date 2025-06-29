6a37fe3c476c77466c5b6375b83e0f94977fa8d8
Sreerang <mandpesreerang@gmail.com>
2025-06-29 17:34:56 +0530

    bb

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -3,18 +3,20 @@ import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { AppStage } from './app-stage';
 import * as cdk from 'aws-cdk-lib';
+import { GitHubTrigger } from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    console.log("lk",cdk.SecretValue.secretsManager('arn:aws:secretsmanager:ap-south-1:807157871082:secret:githubtoken-G3usqh'));
+    //console.log("lk",cdk.SecretValue.secretsManager('GITHUB_TOKEN'));
     
 
     const pipeline = new CodePipeline(this, 'MyPipeline', {
       pipelineName: 'SimplePipeline1',
       synth: new ShellStep('SynthStep', {
         input: CodePipelineSource.gitHub('Sreerang15/cdk-codepipeline-demo', 'master', {
-          authentication: cdk.SecretValue.secretsManager('arn:aws:secretsmanager:ap-south-1:807157871082:secret:githubtoken-G3usqh'),
+          authentication: cdk.SecretValue.secretsManager('GITHUB_TOKEN'),
+          trigger: GitHubTrigger.NONE
         }),
         commands: [
           'npm ci',
