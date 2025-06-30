0e69345c039444ab7fc051d2f5596b0bf40297dd
Sreerang <mandpesreerang@gmail.com>
2025-06-30 17:12:45 +0530

    mmm

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,6 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, CodeBuildStep } from 'aws-cdk-lib/pipelines';
 import { AppStage } from './app-stage';
 import * as cdk from 'aws-cdk-lib';
 import { GitHubTrigger } from 'aws-cdk-lib/aws-codepipeline-actions';
@@ -10,22 +10,29 @@ const s = "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"
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    //console.log("lk",cdk.SecretValue.secretsManager('GITHUB_TOKEN'));
-    
 
     const pipeline = new CodePipeline(this, 'MyPipeline', {
       pipelineName: 'SimplePipeline2',
-      synth: new ShellStep('SynthStep', {
+      synth: new CodeBuildStep('SynthStep', {
         input: CodePipelineSource.gitHub('Sreerang15/cdk-codepipeline-demo', 'master', {
           authentication: cdk.SecretValue.unsafePlainText(s),
           trigger: GitHubTrigger.NONE
         }),
+        installCommands: [
+          'npm ci'
+        ],
         commands: [
-          'npm ci',
           'npm run build',
           'npx cdk synth'
         ],
-      }),
+        // Optional: if you want to give build project more permissions
+        rolePolicyStatements: [
+          new cdk.aws_iam.PolicyStatement({
+            actions: ['*'],
+            resources: ['*']
+          })
+        ]
+      })
     });
 
     pipeline.addStage(new AppStage(this, 'AppStage'));
